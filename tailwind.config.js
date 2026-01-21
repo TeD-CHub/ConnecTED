4aec9c284b7785abf5b0934f829b2d6e833e2eed
TeD-CHub <teddychurchill682@gmail.com>
2026-01-21 16:41:29 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,tsx}", "./*.html"], // Include root html files too
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': '#0A2A4E',
+        'primary-accent': '#00BFFF',
+        'primary-text': '#1D2C38',
+        'light-bg': '#F4F7F9',
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        display: ['Montserrat', 'system-ui', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
