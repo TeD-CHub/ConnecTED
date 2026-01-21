7cb785a49949f7db36aafccdaaa66b4736a07a62
TeD-CHub <teddychurchill682@gmail.com>
2026-01-21 14:34:08 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // update this if your HTML/JS is elsewhere
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
