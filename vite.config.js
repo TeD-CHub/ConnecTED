72ba484c7368b2105725d7325ad562c9590530b2
TeD-CHub <teddychurchill682@gmail.com>
2026-02-04 15:19:44 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,43 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            },
+            manifest: {
+                name: "ConnecTED",
+                short_name: "ConnecTED",
+                icons: [
+                    {
+                        src: "/web-app-manifest-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        purpose: "maskable"
+                    },
+                    {
+                        src: "/web-app-manifest-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable"
+                    }
+                ],
+                theme_color: "#ffffff",
+                background_color: "#ffffff",
+                display: "standalone",
+                start_url: "/",
+                scope: "/"
+            }
+        })
+    ],
     build: {
         rollupOptions: {
             input: {
